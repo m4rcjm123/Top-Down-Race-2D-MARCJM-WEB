aaeb9573b5a85b077fb54b0c8b64f4280e956b39
Marc <m4rcjm123@gmail.com>
2025-05-21 09:34:14 +0200

    a

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     var container = document.querySelector("#unityContainer");
     createUnityInstance(container, {
-      dataUrl: "Build/game.data",
-      frameworkUrl: "Build/game.framework.js",
-      codeUrl: "Build/game.wasm",
+      dataUrl: "Build/BuildWebGL.data",
+      frameworkUrl: "Build/BuildWebGL.framework.js",
+      codeUrl: "Build/BuildWebGL.wasm",
     }).then((unityInstance) => {
       console.log("Juego cargado");
     }).catch((error) => {
